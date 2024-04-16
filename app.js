2b032fa0ff909f1f6d8be85f7be512d94e0b65c3
Tomasz Turek <turek9933@gmail.com>
2024-04-16 14:19:35 +0200

    Dziala

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
+const http = require('http');
+const routes = require('./routes/index');
+const PORT = 3000;
 
+const requestListener = (req, res) => {
+    if (req.url === '/' && req.method === 'GET') {
+        routes.handleHome(req, res);
+    }
+    else if (req.url === '/add-car') {
+        console.log(req.method);
+        console.log(req.url);
+        console.log("!!!!!!!!!!!!!");
+        routes.handleAddCar(req.method, req, res);
+    }
+    else if (req.url === '/car' && req.method === 'GET') {
+        routes.handleCar(req, res);
+    }
+    else {
+        routes.handlePageNotFound(req, res);
+    }
+}
+
+const server = http.createServer(requestListener);
+
+server.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}.`);
+});
